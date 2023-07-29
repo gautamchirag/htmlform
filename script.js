807e78a779a12bf80c5519efa8e714e9837be94a
Chirag_Gautam <chiraggautam8447@gmail.com>
2023-07-29 17:01:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function validateForm() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const country = document.getElementById('country').value;
+
+  if (name === '' || email === '' || password === '' || country === '') {
+    alert('All fields are required');
+    return false;
+  }
+
+  // You can add more validation checks here, like email format validation, etc.
+
+  return true;
+}
